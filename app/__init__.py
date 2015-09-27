243fdd0c2e771bf4566e112ba63d6cece810408a
Coderhypo <i@ihypo.net>
2015-09-27 11:57:54 +0800

    add ,gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin
 
 import os
 from config import config
 
 app = Flask(__name__)
 db = SQLAlchemy()
-
+admin = Admin()
 
 with app.app_context():
     config_name = os.getenv('CONFIG') or 'default'
@@ -14,6 +15,6 @@ with app.app_context():
     config[config_name].init_app(app)
 
     db.init_app(app)
-
+    admin.init_app(app)
 
 from .views import index
