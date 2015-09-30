8dfd8404577e6596395664f62560ea2eaa82af1e
Coderhypo <i@ihypo.net>
2015-10-01 00:12:51 +0800

    finish page

--- app/models/pages.py
+++ app/models/pages.py
@@ -6,15 +6,20 @@ class Page(db.Model):
     '''页面表'''
 
     __tablename__ = 'pages'
-    page_id = db.Column(db.Integer,primary_key=True)
-    page_title = db.Column(db.Text,nullable=False)
+    page_id = db.Column(db.Integer, primary_key=True)
+    page_title = db.Column(db.Text, nullable=False)
     page_content = db.Column(db.Text)
     page_status = db.Column(db.Integer)
-    page_password = db.Column(db.String)
-    page_date = db.Column(db.Date,nullable=False)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.user_id'))
+    page_password = db.Column(db.String(64))
+    page_date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
     def __repr__(self):
         return '<Page % r>' % self.page_id
 
-
+PageStatus = {
+    'UNAUDITED': 1,  # 未审核状态
+    'DRAFT': 2,      # 草稿状态
+    'RELEASED': 3,   # 已发布
+    'PRIVATE': 4     # 私有状态
+}
