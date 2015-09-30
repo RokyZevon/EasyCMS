c6a2a61a2ce562c9dc2f85bd65fb99f625452fa6
Coderhypo <i@ihypo.net>
2015-10-01 00:13:32 +0800

    finish users

--- app/models/users.py
+++ app/models/users.py
@@ -6,16 +6,26 @@ class User(db.Model):
     '''用户表'''
 
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer,primary_key=True)
-    user_login = db.Column(db.String,unique=True)
-    user_nicename = db.Column(db.String,unique=True)
-    user_pass = db.Column(db.String)
-    user_email = db.Column(db.String)
-    user_url = db.Column(db.String)
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_login = db.Column(db.String(64), unique=True, nullable=False)
+    user_nicename = db.Column(db.String(64), unique=True)
+    user_pass = db.Column(db.String(64), nullable=False)
+    user_email = db.Column(db.String(64), unique=True, nullable=False)
+    user_url = db.Column(db.String(64))
     user_rule = db.Column(db.Integer)
 
-    posts = db.relationship('Post',backref='users')
-    pages = db.relationship('Page',backref='Users')
+    posts = db.relationship('Post', backref='user')
+    pages = db.relationship('Page', backref='user')
 
     def __repr__(self):
         return '<Users %r>' % self.user_id
+
+    def __init__(self):
+        pass
+
+UserRule = {
+    'ADMIN': 1,   # 管理员
+    'EDITOR': 2,  # 编辑
+    'AUTHER': 3,  # 作者
+    'READER': 4   # 订阅者，读者
+}
