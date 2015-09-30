0af455b7881c2cc251f1c557d3aac44127fb9c7f
Coderhypo <i@ihypo.net>
2015-10-01 00:13:07 +0800

    finish posts

--- app/models/posts.py
+++ app/models/posts.py
@@ -6,16 +6,23 @@ class Post(db.Model):
     '''文章表'''
 
     __tablename__ = 'posts'
-    post_id = db.Column(db.Integer,primary_key=True)
-    post_title = db.Column(db.Text,nullable=False)
+    post_id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.Text, nullable=False)
     post_content = db.Column(db.Text)
-    post_status = db.Column(db.Integer,nullable=False)
-    post_password = db.Column(db.String)
-    post_date = db.Column(db.DateTime,nullable=False)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.user_id'))
+    post_status = db.Column(db.Integer, nullable=False)
+    post_password = db.Column(db.String(64))
+    post_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
-    postmetas = db.relationship('Postmeta',backref='posts')
-    postlabels = db.relationship('Postlabel',backref='posts')
+    postmetas = db.relationship('Postmeta', backref='post')
+    postlabels = db.relationship('Postlabel', backref='post')
 
     def __repr__(self):
         return '<Post % r>' % self.post_id
+
+PostStatus = {
+    'UNAUDITED': 1,  # 未审核状态
+    'DRAFT': 2,      # 草稿状态
+    'RELEASED': 3,   # 已发布
+    'PRIVATE': 4     # 私有状态
+}
