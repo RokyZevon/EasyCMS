3308d95a9123cea2817de45e1e129fd9fd9c5927
Coderhypo <i@ihypo.net>
2015-10-01 00:17:04 +0800

    start create some func about catch data in DB

--- app/catch/postcatch.py
+++ app/catch/postcatch.py
@@ -1,1 +1,28 @@
-__author__ = 'hypo'
+# coding=utf-8
+
+from app import db
+
+# 以ID获取文章
+def getPostById(postId):
+    pass
+
+# 获取全部文章
+def getAllPosts():
+    pass
+
+# 获取某人文章
+def getPostsByUserId(userId):
+    pass
+
+# 获取某个分类文章
+def getPostsByMetaId(metaId):
+    pass
+
+# 获取某个标签文章
+def getPostsByLabelId(labelId):
+    pass
+
+# 随机获得文章
+def getPostsRand():
+    pass
+
