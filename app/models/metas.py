7714afd4e04993f8e743a2ac7195969d12e3cd6b
Coderhypo <i@ihypo.net>
2015-10-01 00:12:34 +0800

    finish metas

--- app/models/metas.py
+++ app/models/metas.py
@@ -6,23 +6,24 @@ class Meta(db.Model):
     '''文章分类表'''
 
     __tablename__ = 'metas'
-    meta_id = db.Column(db.Integer,primary_key=True)
-    meta_name = db.Column(db.String)
-    meta_slug = db.Column(db.String,unique=True)
+    meta_id = db.Column(db.Integer, primary_key=True)
+    meta_name = db.Column(db.String(64), nullable=False, unique=True)
+    meta_slug = db.Column(db.String(64), nullable=False, unique=True)
     meta_describe = db.Column(db.Text)
 
-    postmetas = db.relationship('Postmeta',backref='metas')
+    postmetas = db.relationship('Postmeta',backref='meta')
 
     def __repr__(self):
         return '<Meta %r>' % self.meta_id
 
-class PostMeta(db.Model):
+class Postmeta(db.Model):
 
     '''文章分类明细表'''
 
     __tablename__ = 'postmetas'
-    meta_id = db.Column(db.Integer,db.ForeignKey('metas.meta_id'))
-    post_id = db.Column(db.Integer,db.ForeignKey('posts.post_id'))
+    id = db.Column(db.Integer, primary_key=True)
+    meta_id = db.Column(db.Integer, db.ForeignKey('metas.meta_id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
 
     def __repr__(self):
         return '<PostMeta postId:%r = metaId:%r>' % (self.post_id,self.meta_id)
