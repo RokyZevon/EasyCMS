ee3a17cd0aac847e21e4265ed86a5070ccf9dd95
Coderhypo <i@ihypo.net>
2015-10-01 00:12:13 +0800

    finish labels

--- app/models/labels.py
+++ app/models/labels.py
@@ -6,10 +6,11 @@ class Label(db.Model):
     '''标签表'''
 
     __tablename__ = 'labels'
-    label_id = db.Column(db.Integer,primary_key=True)
-    label_name = db.Column(db.String,nullable=False)
+    label_id = db.Column(db.Integer, primary_key=True)
+    label_name = db.Column(db.String(60), nullable=False, unique=True)
+    label_slug = db.Column(db.String(60), nullable=False, unique=True)
 
-    postlabels = db.relationship('Postlabel',backref='labels')
+    postlabels = db.relationship('Postlabel', backref='label')
 
     def __repr__(self):
         return '<Label %r>' % self.label_id
@@ -19,8 +20,9 @@ class Postlabel(db.Model):
     '''文章，标签对应表'''
 
     __tablename__ = 'psotlabels'
-    label_id = db.Column(db.Integer,db.ForeignKey('labels.label_id'))
-    post_id = db.Column(db.Integer,db.ForeignKey('posts.post_id'))
+    id = db.Column(db.Integer, primary_key=True)
+    label_id = db.Column(db.Integer, db.ForeignKey('labels.label_id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
 
     def __repr__(self):
         return '<Postlabel postId:%r == labelId:%r>' % (self.post_id,self.label_id)
