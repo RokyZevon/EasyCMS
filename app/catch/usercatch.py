3dee074be00df2fe3dad2d5c3800467c7d6e039f
Coderhypo <i@ihypo.net>
2015-10-03 00:06:41 +0800

    finish usercatch

--- app/catch/usercatch.py
+++ app/catch/usercatch.py
@@ -1,21 +1,33 @@
 # coding=utf-8
 
+from ..models import User
+
 # 根据ID获取用户
 def getUserById(userId):
-    pass
+
+    user = User.query.filter_by(user_id=userId).first()
+    return user
 
 # 根据邮箱获取用户
 def getUserEmail(Email):
-    pass
+
+    user = User.query.filter_by(user_email=Email).first()
+    return user
 
 # 根据登录名获取用户
 def getUserByLoginName(loginName):
-    pass
+
+    user = User.query.filter_by(user_login=loginName).first()
+    return user
 
 # 根据权限获取用户组
 def getUsersByRule(userRule):
-    pass
+
+    user = User.query.filter_by(user_rule=userRule).all()
+    return user
 
 # 获取全部用户
 def getAllUsers():
-    pass
+
+    userList = User.query.all()
+    return userList
