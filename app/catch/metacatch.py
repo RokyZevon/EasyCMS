505ecf70911f925a7b0cae42db60132d5548e648
Coderhypo <i@ihypo.net>
2015-10-03 00:05:57 +0800

    finish metacatch

--- app/catch/metacatch.py
+++ app/catch/metacatch.py
@@ -1,1 +1,31 @@
 # coding=utf-8
+
+from ..models import Meta, Postmeta
+
+# 获得全部分类
+def getAllMetas():
+
+    metaList = Meta.query.all()
+
+    return metaList
+
+# 根据分类ID获得分类
+def getMetaById(metaId):
+
+    meta = Meta.query.filter_by(meta_id=metaId).first()
+
+    return meta
+
+# 根据分类缩写获得分类
+def getMetaBySlug(slug):
+
+    meta = Meta.query.filter_by(meta_slug=slug).first()
+
+    return meta
+
+# 获得文章的全部分组
+def getPostMetas(postId):
+
+    metaList = Postmeta.query.filter_by(post_id=postId).all()
+
+    return metaList
