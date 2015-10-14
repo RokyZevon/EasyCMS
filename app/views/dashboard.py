0eaa7eb13cbe7c795e05a24c30db11a09050aed2
Coderhypo <i@ihypo.net>
2015-10-14 15:36:10 +0800

    add /admin/users

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -32,3 +32,27 @@ def editpost():
     editForm.status.choices = [(PostStatus['RELEASED'], u'已发布'), (PostStatus['DRAFT'], u'草稿'), (PostStatus['PRIVATE'], u'私有'),]
 
     return render_template('admin/editpost.html', form=editForm)
+
+
+# 仪表盘 新建文章 编辑文章
+@app.route('/admin/users')
+@login_required
+def useredit():
+
+    userList = getAllUsers()
+
+    list = []
+    rule = {0: u'用户被禁用', 1: u'订阅者', 2: u'作者', 3: u'编辑', 4: u'管理员'}
+    label = {0: 'label-default', 1: 'label-info', 2: 'label-primary', 3: 'label-success', 4: 'label-warning'}
+    for user in userList:
+        tmp = {}
+        tmp['id'] = user.user_id
+        tmp['login'] = user.user_login
+        tmp['nicename'] = user.user_nicename
+        tmp['email'] = user.user_email
+        tmp['rule'] = rule[user.user_rule]
+        tmp['label'] = label[user.user_rule]
+
+        list.append(tmp)
+
+    return render_template('admin/users.html', list=list)
