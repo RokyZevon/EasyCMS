a7a16e3560c45668bcdce7120a2b44d84c82def5
Coderhypo <i@ihypo.net>
2015-10-15 19:57:00 +0800

    update user model and post model

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -34,10 +34,46 @@ def editpost():
     return render_template('admin/editpost.html', form=editForm)
 
 
-# 仪表盘 新建文章 编辑文章
-@app.route('/admin/users')
+# 仪表盘 用户管理
+@app.route('/admin/users', methods=['GET', 'POST'])
 @login_required
 def useredit():
+
+    addForm = AddUserForm()
+    editForm = EditUserForm()
+    delForm = DelUserForm()
+
+    if request.method == 'POST':
+
+        if addForm.validate_on_submit():
+            userInfo = {}
+            userInfo['login'] = addForm.addLogin.data
+            userInfo['email'] = addForm.addEmail.data
+            userInfo['nicename'] = addForm.addNicename.data
+            userInfo['pass'] = addForm.addPassword.data
+            userInfo['rule'] = int(addForm.addRule.data)
+            addUser(userInfo)
+            flash(u"注册成功，请验证邮箱后登录！")
+
+        if editForm.validate_on_submit():
+            userInfo = {}
+            userInfo['id'] = editForm.editId.data
+            userInfo['login'] = editForm.editLogin.data
+            userInfo['nicename'] = editForm.editNicename.data
+            userInfo['url'] = editForm.editUrl.data
+            userInfo['pass'] = editForm.editPassword.data
+            userInfo['rule'] = int(editForm.editRule.data)
+            addUser(userInfo)
+            flash(u"用户信息修改成功！")
+
+        if delForm.validate_on_submit():
+            user = getUserById(delForm.delId.data)
+            if user.user_login == delForm.delLogin.data:
+                delUser(int(delForm.delId.data))
+                flash(u"用户删除成功！")
+            else:
+                flash(u"删除失败！")
+
 
     userList = getAllUsers()
 
@@ -57,9 +93,4 @@ def useredit():
 
         list.append(tmp)
 
-    addForm = AddUserForm()
-    addForm.addRule.choices = [("1", u"订阅者"), ("2", u"作者"), ("3", u"编辑"), ("4", u"管理员"), ("0", u"未激活用户")]
-    editFrom = EditUserFrom()
-    editFrom.editRule.choices = [("1", u"订阅者"), ("2", u"作者"), ("3", u"编辑"), ("4", u"管理员"), ("0", u"禁用用户")]
-
-    return render_template('admin/users.html', list=list, addForm=addForm, editFrom=editFrom)
+    return render_template('admin/users.html', list=list, addForm=addForm, editForm=editForm, delForm=delForm)
