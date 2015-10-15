b40b5024f39f151636433f53d490e3aa46582b64
Coderhypo <i@ihypo.net>
2015-10-15 19:57:34 +0800

    add userEdit userAdd delUser Fomr

--- app/views/forms.py
+++ app/views/forms.py
@@ -1,12 +1,13 @@
 # coding=utf-8
 
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, \
-    BooleanField, SubmitField, ValidationError, HiddenField, \
-    DateTimeField
-from wtforms.fields import SelectField
-from flask.ext.pagedown.fields import PageDownField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms import BooleanField, ValidationError, HiddenField
+from wtforms import DateTimeField, SelectField, SelectMultipleField
+
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from flask.ext.pagedown.fields import PageDownField
+
 from ..catch import getUserByLoginName, getUserByEmail
 from ..models import PostStatus
 
@@ -40,24 +41,29 @@ class RegisterForm(Form):
 
 # 个人中心修改个人信息表单
 class EditUserInfoFrom(Form):
-    login = HiddenField()
-    nicename = StringField(u'昵称', validators=[Required(), Length(1, 64)])
-    url = StringField(u'个人主页', validators=[Required(), Length(1, 64)])
-    submit = SubmitField(u'更新资料')
+    editILogin = HiddenField()
+    editINicename = StringField(u'昵称', validators=[Required(), Length(1, 64)])
+    editIUrl = StringField(u'个人主页', validators=[Required(), Length(0, 64)])
+    editISubmit = SubmitField(u'更新资料')
+
+    def validate_editUrl(self, field):
+        if field.data == 'http://':
+            field.data = ''
 
 
 # 个人中心修改个人密码表单
 class EditUserPassFrom(Form):
-    login = HiddenField()
-    password = PasswordField(u'密码', validators=[Required(), EqualTo('password2', message='两次输入的密码不一致！')])
-    password2 = PasswordField(u'确认密码', validators=[Required()])
-    submit = SubmitField(u'更新密码')
+    editLogin = HiddenField()
+    editPpassword = PasswordField(u'密码', validators=[Required(), EqualTo('password2', message='两次输入的密码不一致！')])
+    editPassword2 = PasswordField(u'确认密码', validators=[Required()])
+    editSubmit = SubmitField(u'更新密码')
 
 
 # 个人中心禁用用户表单
 class UnableUserFrom(Form):
-    login = HiddenField()
-    submit = SubmitField(u'确认禁用')
+    unableLogin = HiddenField()
+    unableName = StringField(u'确认登录名', validators=[Required(), Length(1, 64)])
+    unableSubmit = SubmitField(u'确认禁用')
 
 
 # 仪表盘 文章提交表单
@@ -66,8 +72,8 @@ class EditPostForm(Form):
     content = PageDownField(u'编辑内容', validators=[Required()])
     datetime = DateTimeField(u'发表时间', validators=[Required()])
     password = PasswordField(u'密码', validators=[Required(), Length(0, 64)])
-    status = SelectField(u'状态', default=[(PostStatus['RELEASED'], u"已发布")])
-    metas = SelectField(u'分类目录', default=[("1", u"默认分类")])
+    status = SelectField(u'状态', coerce=int, default=PostStatus['RELEASED'])
+    metas = SelectField(u'分类目录', coerce=int, default=1)
     labels = StringField(u'标签', validators=[Required(), Length(1, 64)])
     submit = SubmitField(u'发布')
     save = SubmitField(u'保存')
@@ -86,37 +92,40 @@ class AddUserForm(Form):
     addEmail = StringField(u'常用邮箱', validators=[Required(), Length(1, 64), Email()])
     addNicename = StringField(u'昵称', validators=[Required(), Length(1, 64)])
     addPassword = PasswordField(u'密码', validators=[Required()])
-    addRule = SelectField(u'用户权限', default=[("1", u"订阅者")])
-    addSubmit = SubmitField(u'添加新账户')
+    addRule = SelectField(u'用户权限', coerce=int, choices=[(1, u"订阅者"), (2, u"作者"), (3, u"编辑"), (4, u"管理员"), (0, u"未激活用户")])
+    addSubmit = SubmitField(u'添加用户')
 
-    def validate_email(self, field):
-        if getUserByEmail(field.data):
-            raise ValidationError('邮箱已被使用！')
-
-    def validate_login(self, field):
-        if getUserByLoginName(field.data):
-            raise ValidationError('登录名已被使用！')
+    # def validate_addEmail(self, field):
+    #     if getUserByEmail(field.data):
+    #         raise ValidationError('邮箱已被使用！')
+    #
+    # def validate_addLogin(self, field):
+    #     if getUserByLoginName(field.data):
+    #         raise ValidationError('登录名已被使用！')
 
 
 # 仪表盘 修改用户提交表单
-class EditUserFrom(Form):
+class EditUserForm(Form):
     editId = HiddenField()
     editLogin = StringField(u'登录名', validators=[Required(), Length(1, 64),
                                                Regexp('^[A-Za-z][A-Za-z0-9_]*$', 0, '登录名只允许是字母，数字，下划线')])
     editNicename = StringField(u'昵称', validators=[Required(), Length(1, 64)])
-    editPassword = PasswordField(u'密码', validators=[Required()])
-    editUrl = StringField(u'个人主页', validators=[Required(), Length(1, 64)])
-    editRule = SelectField(u'用户权限', default=[("1", u"订阅者")])
+    editPassword = PasswordField(u'密码')
+    editUrl = StringField(u'个人主页', validators=[Length(0, 64)])
+    editRule = SelectField(u'用户权限', coerce=int, choices=[(1, u"订阅者"), (2, u"作者"), (3, u"编辑"), (4, u"管理员"), (0, u"禁用用户")],default=1)
     editSubmit = SubmitField(u'更新资料')
 
-    def validate_login(self, field):
-        if getUserByLoginName(field.data):
-            raise ValidationError('登录名已被使用！')
+    def validate_editUrl(self, field):
+        if field.data == 'http://':
+            field.data = ''
+
 
 
 # 仪表盘 删除用户提交表单
-class DelUser(Form):
+class DelUserForm(Form):
     delId = HiddenField()
+    delLogin = StringField(u'确认登录名', validators=[Required(), Length(1, 64),
+                                               Regexp('^[A-Za-z][A-Za-z0-9_]*$', 0, '登录名只允许是字母，数字，下划线')])
     delSubmit = SubmitField(u'确认删除')
 
 
