50a864e7f7c34fadef582782d2ff348522c341c6
Coderhypo <i@ihypo.net>
2015-10-18 00:49:20 +0800

    add page slug unique=True and del page status

--- app/models/pages.py
+++ app/models/pages.py
@@ -8,8 +8,9 @@ class Page(db.Model):
     __tablename__ = 'pages'
     page_id = db.Column(db.Integer, primary_key=True)
     page_title = db.Column(db.Text, nullable=False)
-    page_slug = db.Column(db.Text, nullable=False)
+    page_slug = db.Column(db.Text, nullable=False, unique=True)
     page_content = db.Column(db.Text)
+    page_content_html = db.Column(db.Text)
     page_status = db.Column(db.Integer)
     page_password = db.Column(db.String(64))
     page_date = db.Column(db.Date, nullable=False)
@@ -17,10 +18,3 @@ class Page(db.Model):
 
     def __repr__(self):
         return '<Page % r>' % self.page_id
-
-PageStatus = {
-    'UNAUDITED': 1,  # 未审核状态
-    'DRAFT': 2,      # 草稿状态
-    'RELEASED': 3,   # 已发布
-    'PRIVATE': 4     # 私有状态
-}
