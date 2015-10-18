6a5723c97f8402986084155e10a51d4293884991
Coderhypo <i@ihypo.net>
2015-10-18 13:13:06 +0800

    add listen about html

--- app/models/pages.py
+++ app/models/pages.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from app import db
+from markdown import markdown
+import bleach
 
 class Page(db.Model):
 
@@ -16,5 +18,16 @@ class Page(db.Model):
     page_date = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
+    @staticmethod
+    def on_changed_page_content(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
+                        'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'p']
+        target.page_content_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tars=allowed_tags, strip=True
+        ))
+
     def __repr__(self):
         return '<Page % r>' % self.page_id
+
+db.event.listen(Page.page_content, 'set', Page.on_changed_page_content)
