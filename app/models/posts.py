0c3968f4faafa9dc78e10d15da3356f65a986027
Coderhypo <i@ihypo.net>
2015-10-18 13:13:37 +0800

    add listen about html

--- app/models/posts.py
+++ app/models/posts.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from app import db
+from markdown import markdown
+import bleach
 
 class Post(db.Model):
 
@@ -18,8 +20,19 @@ class Post(db.Model):
     postmetas = db.relationship('Postmeta', backref='post')
     postlabels = db.relationship('Postlabel', backref='post')
 
+    @staticmethod
+    def on_changed_post_content(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
+                        'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'p']
+        target.post_content_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tars=allowed_tags, strip=True
+        ))
+
     def __repr__(self):
         return '<Post % r>' % self.post_id
+
+db.event.listen(Post.post_content, 'set', Post.on_changed_post_content)
 
 PostStatus = {
     'UNAUDITED': 1,  # 未审核状态
