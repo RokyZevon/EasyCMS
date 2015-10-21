14a90502d930d988596343c7a9469133b6624c83
Coderhypo <i@ihypo.net>
2015-10-21 14:20:46 +0800

    update submetal

--- app/factory/subpost.py
+++ app/factory/subpost.py
@@ -1,31 +1,60 @@
 # -*- coding=utf-8 -*-
 
 from app import db
-from ..models import Postmeta, Postlabel, Post
+from ..models import Postmeta, Postlabel
+from ..catch import getLabelById, getMetaById
 
 # 文章设置文章分组 and 文章更新文章分类
 def setPostMeta(metaInfo):
 
     postId = metaInfo['id']
-    meta = metaInfo['list']
+    metaid = metaInfo['list']
+
+    delmeta = Postmeta.query.filter_by(post_id=postId).first()
+    if delmeta:
+        umeta = getMetaById(delmeta.meta_id)
+        umeta.meta_num -= 1
+        db.session.add(umeta)
 
     db.session.query(Postmeta).filter(Postmeta.post_id == postId).delete()
 
     postMeta = Postmeta()
-    postMeta.meta_id = meta
+    postMeta.meta_id = metaid
     postMeta.post_id = postId
 
+    umeta = getMetaById(metaid)
+    umeta.meta_num += 1
+
+    db.session.add(umeta)
     db.session.add(postMeta)
     db.session.commit()
 
+
 # 文章设置文章标签 and 文章更新文章标签
-def setPostLabel(labelInfo):
+def setPostLabel(labelinfo):
 
-    postId = labelInfo['id']
-    labelList = labelInfo['list']
+    postid = labelinfo['id']
+    labellist = labelinfo['list']
 
-    print postId
-    print labelList
+    dellabellist = Postlabel.query.filter_by(post_id=postid).all()
+
+    for dellabel in dellabellist:
+        label = getLabelById(dellabel.label_id)
+        label.label_num -= 1
+        db.session.add(label)
+
+    db.session.query(Postlabel).filter(Postlabel.post_id == postid).delete()
+
+    for label in labellist:
+        postlabel = Postlabel()
+        postlabel.label_id = label.label_id
+        postlabel.post_id = postid
+        label.label_num += 1
+        db.session.add(label)
+        db.session.add(postlabel)
+
+    db.session.commit()
+
 
 # 删除文章分组信息
 def delPostMetas(postId):
