83e71fc69b9d2d3ed730f69b2e10381b7373d8bf
Coderhypo <i@ihypo.net>
2015-10-28 00:06:02 +0800

    metas FK to posts

--- app/models/metas.py
+++ app/models/metas.py
@@ -12,19 +12,7 @@ class Meta(db.Model):
     meta_num = db.Column(db.Integer, nullable=False)
     meta_describe = db.Column(db.Text)
 
-    postmetas = db.relationship('Postmeta',backref='meta')
+    posts = db.relationship('Post', backref='metas')
 
     def __repr__(self):
         return '<Meta %r>' % self.meta_id
-
-class Postmeta(db.Model):
-
-    '''文章分类明细表'''
-
-    __tablename__ = 'postmetas'
-    id = db.Column(db.Integer, primary_key=True)
-    meta_id = db.Column(db.Integer, db.ForeignKey('metas.meta_id'))
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
-
-    def __repr__(self):
-        return '<PostMeta postId:%r = metaId:%r>' % (self.post_id, self.meta_id)
