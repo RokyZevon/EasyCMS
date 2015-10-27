a8b9590329da10d7afe6e906e4b7de03b353a9e2
Coderhypo <i@ihypo.net>
2015-10-28 00:03:35 +0800

    rename and update the way catch label

--- app/catch/labelcatch.py
+++ app/catch/labelcatch.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from ..models import Label, Postlabel
-from .postcatch import getPostsByLabelId
+from .postcatch import get_posts_by_label_id
 
 
 # 获得全部标签
@@ -18,9 +18,9 @@ def get_all_label(page=1, num=5):
 
 
 # 根据标签ID获得标签
-def getLabelById(labelId):
+def get_label_by_id(labelid):
 
-    label = Label.query.filter_by(label_id=labelId).first()
+    label = Label.query.filter_by(label_id=labelid).first()
 
     return label
 
@@ -48,12 +48,12 @@ def getPostLabels(postId):
 # 获得带文章数的标签（根据标签文章数，可做标签云）
 def getLabelOrder():
 
-    labelList = getAllLabel()
+    labelList = get_all_label()
 
     list = []
 
     for label in labelList:
-        list.append({'label': label, 'num': len(getPostsByLabelId(label.label_id))})
+        list.append({'label': label, 'num': len(get_posts_by_label_id(label.label_id))})
 
     return list
 
