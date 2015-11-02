3b87b4318d2686e9e114142eb29aec737e2a30ce
Coderhypo <i@ihypo.net>
2015-11-02 11:37:40 +0800

    update get labels

--- app/catch/labelcatch.py
+++ app/catch/labelcatch.py
@@ -25,25 +25,30 @@ def get_label_by_id(labelid):
     return label
 
 # 根据标签缩写获得标签
-def getLabelBySlug(slug):
+def get_label_by_slug(slug):
 
     label = Label.query.filter_by(label_slug=slug).first()
 
     return label
 
 # 根据标签缩写获得标签
-def getLabelByName(name):
+def get_label_by_name(name):
 
     label = Label.query.filter_by(label_name=name).first()
 
     return label
 
 # 获得文章的全部标签
-def getPostLabels(postId):
+def get_post_labels(postid):
 
-    labelList = Postlabel.query.filter_by(post_id=postId).all()
+    labellist = Postlabel.query.filter_by(post_id=postid).all()
+    rnt = []
 
-    return labelList
+    for postlabel in labellist:
+        label = get_label_by_id(postlabel.label_id)
+        rnt.append(label)
+
+    return rnt
 
 # 获得带文章数的标签（根据标签文章数，可做标签云）
 def getLabelOrder():
